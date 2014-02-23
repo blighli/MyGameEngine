aadcfe3872ed148398a40914cbae916fc86ba68e
Li Qilei <blighli@gmail.com>
2014-02-23 13:32:49 +0800

    add ui files

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
-int main()
+#include <QApplication>
+#include <QDialog>
+
+#include "ui_DisplayWidget.h"
+
+int main(int argc, char* argv[])
 {
-	return 0;
+	QApplication app(argc, argv);
+	Ui::Form form;
+	QDialog* dialog = new QDialog();
+	form.setupUi(dialog);
+	dialog->show();
+	return app.exec();
 }
